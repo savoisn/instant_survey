9b222fc233e4cd40af67af9fe1cb927eed8145a8
Nicolas Savois <nicolas.savois@gmail.com>
2022-07-06 10:08:30 +0200

    added channel for survey control

--- lib/instant_survey_web/channels/survey_channel.ex
+++ lib/instant_survey_web/channels/survey_channel.ex
@@ -1,7 +1,20 @@
 defmodule InstantSurveyWeb.SurveyChannel do
   use InstantSurveyWeb, :channel
 
+  alias Phoenix.PubSub
   alias InstantSurveyWeb.Presence
+  alias InstantSurvey.Game
+
+  @impl true
+  def join("survey:" <> channel_id = topic, payload, socket) do
+    send(self(), {:after_join, channel_id})
+
+    if authorized?(payload) do
+      {:ok, %{channel_id: channel_id}, assign(socket, :channel_id, topic)}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
 
   @impl true
   def join("survey:lobby", payload, socket) do
@@ -15,12 +28,13 @@ defmodule InstantSurveyWeb.SurveyChannel do
   end
 
   @impl true
-  def handle_info(:after_join, socket) do
+  def handle_info({:after_join, channel_id}, socket) do
     IO.inspect(socket.assigns)
 
     {:ok, _} =
       Presence.track(socket, socket.assigns["name"], %{
-        online_at: inspect(System.system_time(:second))
+        online_at: inspect(System.system_time(:second)),
+        channel_id: channel_id
       })
 
     push(socket, "presence_state", Presence.list(socket))
@@ -39,6 +53,23 @@ defmodule InstantSurveyWeb.SurveyChannel do
   @impl true
   def handle_in("shout", payload, socket) do
     broadcast(socket, "shout", payload)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("push_to_next_question", %{"next_question_id" => question_id}, socket) do
+    IO.inspect(question_id)
+    broadcast(socket, "next_question", %{next_question_id: question_id})
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("display_question_result", %{"question_id" => question_id}, socket) do
+    IO.inspect(question_id)
+    broadcast(socket, "display_question_result", %{question_id: question_id})
+
     {:noreply, socket}
   end
 
